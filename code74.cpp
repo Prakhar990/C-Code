0eef1f974c997d91f1542dfbd85689a8fcb09fce
Prakhar990 <badestaofficial@gmail.com>
2026-08-15 19:36:59 +0530

    Swapping Values By Pass By Value

---
+++ code74.cpp
@@ -0,0 +1,22 @@
+/*SWAPPING VALUES BY PASS BY VALUE*/
+
+#include <iostream>
+using namespace std;
+int swap(int x, int y){
+    int temp=x;
+    x=y;
+    y=temp;
+
+    return x,y;
+}
+
+int main(){
+    int x=10,y=20;
+
+    swap(x,y);
+
+    cout<<"x"<<" "<<x<<endl;
+    cout<<"y"<<" "<<y<<endl;
+
+    return 0;
+}
